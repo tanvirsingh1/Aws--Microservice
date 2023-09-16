7ee9e20c55e033fc0aa4ce730122652178b648bf
tanvirsingh1 <tnvir2182002@gmail.com>
2023-09-16 19:43:28 -0400

    authrnticatin routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
 
+const authenticate = require('./auth');
 // author and version from our package.json file
-const { author, version } = require('../package.json');
 
+
+
+// Create an express app instance we can use to attach middleware and HTTP routes
+const app = express();
+
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 const logger = require('./logger');
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
   logger,
 });
-
-// Create an express app instance we can use to attach middleware and HTTP routes
-const app = express();
 
 // Use pino logging middleware
 app.use(pino);
@@ -29,19 +39,8 @@ app.use(compression());
 
 // Define a simple health check route. If the server is running
 // we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
+app.use('/', require('./routes'));
 
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/tanvirsingh1/fragments',
-    version,
-  });
-});
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
